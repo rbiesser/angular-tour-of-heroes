a67e59de9033c2b912713bc51b5bd8eb14111f07
Robbie Biesser <rbiesser@gmail.com>
2020-07-11 12:16:21 -0400

    use arrow functions

--- controller/hero.js
+++ controller/hero.js
@@ -1,16 +1,16 @@
 // Require the express package and use express.Router()
 var Hero = require('../model/hero')
 
-exports.test = function (req, res) {
-    console.log('test')
-    res.send('Greetings from My Hero controller!')
+exports.test = (req, res) => {
+    console.log("test")
+    res.send("Greetings from My Hero controller!")
 }
 
-exports.create = function (req, res, next) {
-    console.log('create in controller')
-    console.log('body:')
+exports.create = (req, res, next) => {
+    console.log("create in controller")
+    console.log("body:")
     console.log(req.body)
-    console.log('params:')
+    console.log("params:")
     console.log(req.params)
 
     var hero = new Hero(
@@ -20,43 +20,37 @@ exports.create = function (req, res, next) {
         }
     )
 
-    Hero.countDocuments(function (err, count) {
+    // full collection scan
+    Hero.countDocuments((err, count) => {
         hero.id = count++
-        hero.save(function (err) {
-            if (err) {
-                return next(err)
-            }
-
-            console.log('Hero id: ' + hero.id)
-            console.log('Hero name: ' + hero.name)
+        hero.save((err) => {
+            if (err) return next(err)
+            console.log("Hero id: " + hero.id)
+            console.log("Hero name: " + hero.name)
             res.send(hero)
         })
     })
 }
 
-exports.read = function (req, res, next) {
+exports.read = (req, res, next) => {
     console.log("read in controller. id: " + req.params.id)
     Hero.findOne({ id: req.params.id }, (err, hero) => {
-        if (err)
-            return next(err)
+        if (err) return next(err)
         res.json(hero)
     })
 }
 
-exports.heroes = function (req, res, next) {
+exports.heroes = (req, res, next) => {
     console.log("heroes in controller")
     Hero.find({}, (err, docs) => {
-        if (err) {
-            return console.error(err)
-        }
+        if (err) return console.error(err)
         console.log("heroes in controller: " + docs[0])
-        console.log('count: ' + docs.length)
-        // console.log(docs)
+        console.log("count: " + docs.length)
         res.json(docs)
     })
 }
 
-exports.update = function (req, res, next) {
+exports.update = (req, res, next) => {
     console.log("update in controller. id: " + req.body.id)
     Hero.findOneAndUpdate({ id: req.body.id }, { $set: req.body }, (err, hero) => {
         if (err) return next(err)
@@ -64,7 +58,7 @@ exports.update = function (req, res, next) {
     })
 }
 
-exports.delete = function (req, res, next) {
+exports.delete = (req, res, next) => {
     console.log("delete in controller. id: " + req.params.id)
     // Delete by id not _id
     Hero.deleteOne({ id: req.params.id }, (err) => {
